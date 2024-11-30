a41ba9292af567c83d5937ad9a92ec962c35c7bd
Abiolaridwan <127553779+Abiolaridwan@users.noreply.github.com>
2024-12-01 11:16:56 +1300

    Add files via upload

---
+++ src/Details.js
@@ -0,0 +1,194 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/nigeria.png";
+import logo from "./assets/nigeria.png";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import python from "./assets/techstack/python.jpg";
+import canva from "./assets/techstack/canva.png";
+import tensorflow from "./assets/techstack/tensorflow.png";
+// Porject Images
+import projectImage1 from "./assets/projects/Biola_eat.PNG";
+import projectImage2 from "./assets/projects/Biola_build.PNG";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Abiola Ridwan",
+  tagline: "I am a web developer",
+  img: profile,
+  about: `
+  I am a passionate and results-driven web developer with a strong interest in Artificial Intelligence (AI), Machine Learning (ML), and Big Data algorithms, with a particular focus on Large Language Models (LLMs). While my work experience may be evolving, my commitment to excellence, top-notch work ethics, and a relentless pursuit of accuracy and precision set me apart in every project I undertake.
+  
+  Holding a BSc. in Plant Biology and a Postgraduate Diploma (PGD) in Software Engineering, I have successfully transitioned into the tech industry, blending my analytical mindset and problem-solving skills to craft innovative solutions. My diverse background has equipped me with a unique perspective, enabling me to approach complex technical challenges from a multidisciplinary angle.
+  
+  Driven by a deep curiosity and enthusiasm for emerging technologies, I aim to contribute meaningfully to the development and application of AI and machine learning models, while continuing to build on my expertise in web development and software engineering. I thrive in environments that require continuous learning, collaboration, and a commitment to delivering high-quality, scalable solutions.
+`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/yusuf-ridwan-6b3a54206/",
+  github: "https://github.com/Abiolaridwan",
+  twitter: "https://x.com/Abiolaaaaaaa",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Software Engineer",
+    Company: `BEOSMEDIC Limited`,
+    Location: "Nigeria",
+    Type: "Remote",
+    Duration: "Jun 2024 - Present",
+  },
+  {
+    Position: "Software Engineer",
+    Company: `ALYUFAT limited`,
+    Location: "Nigeria",
+    Type: "Remote",
+    Duration: "Feb 2024 - Jun 2024",
+  },
+  {
+    Position: "Web Tutor",
+    Company: `PARACH ICT`,
+    Location: "Nigeria",
+    Type: "Hybrid",
+    Duration: "Nov 2023 - Feb 2024",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Software Engineering",
+    Company: "Lead City University",
+    Location: "Nigeria",
+    Type: "Postgraduate Diploma",
+    Duration: "Oct 2024 - Present",
+  },
+  {
+    Position: "Botany (Plant Biology)",
+    Company: `Ahmadu Bello University`,
+    Location: "Nigeria",
+    Type: "Bachelor of Science",
+    Duration: "Oct 2012 - Jun 2018",
+  },
+];
+
+// Enter your Certification Details here
+export const certDetails = [
+  {
+    Position: "Web Developer",
+    Company: "PARACH ICT",
+    Location: "Nigeria",
+    Type: "Frontend Web Developer",
+    Duration: "Aug 2023 - Nov 2023",
+  }
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  python: python,
+  sass: sass,
+  tailwind: tailwind,
+  tensorflow: tensorflow,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+  canva: canva,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Biola Eat",
+    image: projectImage1,
+    description: `Biola Eat is an innovative food delivery platform designed to connect customers with local restaurants and food vendors. It offers a seamless browsing and ordering experience, ensuring that delicious meals are just a few clicks away. Biola Eat aims to make food delivery faster, more reliable, and accessible while supporting local businesses and fostering a vibrant food community.`,
+    techstack: "HTML, CSS, JavaScript",
+    previewLink: "https://biola-eat.vercel.app/",
+    githubLink: "https://github.com/Abiolaridwan/Biola_Eat",
+  },
+  {
+    title: "Biola Construction",
+    image: projectImage2,
+    description: `Biola.dev is a comprehensive platform designed to support the construction industry. It operates as both a building construction firm and a recruitment agency, connecting employers with skilled, unskilled, and professional personnel while delivering high-quality construction projects to meet diverse client needs.`,
+    techstack: "HTML, CSS, JavaScript",
+    previewLink: "https://construction-seven-orcin.vercel.app/",
+    githubLink: "https://github.com/Abiolaridwan/construction",
+  },
+  {
+    title: "Building Project 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML, CSS, JavaScript",
+    previewLink: "https://github.com/Abiolaridwan",
+    githubLink: "https://github.com/Abiolaridwan",
+  },
+  {
+    title: "Building Project 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML, CSS, JavaScript",
+    previewLink: "https://github.com/Abiolaridwan",
+    githubLink: "https://github.com/Abiolaridwan",
+  },
+  {
+    title: "Project 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML, CSS, JavaScript",
+    previewLink: "https://github.com/Abiolaridwan",
+    githubLink: "https://github.com/Abiolaridwan",
+  },
+  {
+    title: "Project 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML, CSS, JavaScript",
+    previewLink: "https://github.com/Abiolaridwan",
+    githubLink: "https://github.com/Abiolaridwan",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "abiolaridwan993@gmail.com"
+};
